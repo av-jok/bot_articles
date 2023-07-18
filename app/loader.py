6b21041be7b88bea8cfb4902e85387e6279ffb53
Joker <deripasko@gmail.com>
2023-07-18 10:28:01 +0300

    fix

--- app/loader.py
+++ app/loader.py
@@ -2,17 +2,15 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
-from app import config
 from app.config import load_config
 
 conf = load_config("data/.env")
 
 bot = Bot(
-    token=config.BOT_TOKEN,
+    token=conf.tg_bot.token,
     parse_mode=types.ParseMode.HTML,
 )
 
-storage = MemoryStorage()
 storage = RedisStorage2(host=conf.redis.host, db=conf.redis.database) if conf.tg_bot.use_redis else MemoryStorage()
 
 dp = Dispatcher(
