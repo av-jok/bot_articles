e02bb8685f45484db7663676e9ee28638bd84d18
Joker work <deripasko@gmail.com>
2023-09-11 17:12:47 +0300

    fix

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -2,6 +2,7 @@ from loguru import logger
 
 from .errors import retry_after
 from .errors import errors
+from .private import ip
 from .private import state
 from .private import start
 from .private import any
