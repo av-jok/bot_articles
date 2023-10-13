e0292dad8d972a51f13650c75ae2951a65a32491
Joker work <deripasko@gmail.com>
2023-10-13 13:20:39 +0300

    fix

--- api/pynetbox/test.py
+++ api/pynetbox/test.py
@@ -1,13 +1,14 @@
-import base64
-import logging
+# import base64
+# import logging
 import pynetbox
 import urllib3
-import base64
-from django.core.files.base import ContentFile
-from rest_framework import serializers
-from drf_extra_fields.fields import Base64ImageField
+import re
 
-from pprint import pprint
+# from django.core.files.base import ContentFile
+# from rest_framework import serializers
+# from drf_extra_fields.fields import Base64ImageField
+
+# from pprint import pprint
 
 netbox_url = 'https://netbox.avantel.ru/'
 netbox_api = '7f50ada4a4a66d4b2385e4f8f59a069bc219089b'
@@ -16,11 +17,22 @@ urllib3.disable_warnings()
 nb = pynetbox.api(url=netbox_url, token=netbox_api)
 nb.http_session.verify = False
 
-# devices = nb.dcim.devices.filter(asset_tag='17855')
-# for device in devices:
-#     pprint(device)
-#     device.name = device.name.lower()
-#     pprint(dict(device))
+i = 0
+devices = nb.dcim.devices.filter(asset_tag__ic='Инв ', role_id=4)
+for device in devices:
+    if re.findall(r"(?<!\d)\d{5}(?!\d)", device.asset_tag):
+        text = re.findall(r"(?<!\d)\d{5}(?!\d)", device.asset_tag)
+        # device.name = device.name.lower()
+        device.asset_tag = text[0]
+        try:
+            device.save()
+            print(device, device.asset_tag, text)
+        except pynetbox.RequestError as e:
+            print(device.asset_tag, e.error)
+
+        # if i == 29:
+        #     exit()
+        # i += 1
 
 
 # img = nb.extras.image_attachments.filter(object_id='1')
@@ -31,20 +43,20 @@ nb.http_session.verify = False
 #     site.name = site.name.lower()
 #     pprint(dict(site))
 
-with open("joker.png", "rb") as file_handle:
-    image_data = file_handle.read()
-    base_encoded = base64.b64encode(image_data).decode("utf8")
-    image_data2 = dict(
-        content_type="dcim.device",
-        object_id=8593,
-        name="Test image",
-        image=base_encoded,
-        image_height=512,
-        image_width=512,
-        )
-
-    try:
-        nb.extras.image_attachments.create(image_data2)
-    except pynetbox.RequestError:
-        logging.exception(f"Failed to attach image")
+# with open("joker.png", "rb") as file_handle:
+#     image_data = file_handle.read()
+#     base_encoded = base64.b64encode(image_data).decode("utf8")
+#     image_data2 = dict(
+#         content_type="dcim.device",
+#         object_id=8593,
+#         name="Test image",
+#         image=base_encoded,
+#         image_height=512,
+#         image_width=512,
+#         )
+#
+#     try:
+#         nb.extras.image_attachments.create(image_data2)
+#     except pynetbox.RequestError:
+#         logging.exception(f"Failed to attach image")
 # pprint(dict(image_data))
