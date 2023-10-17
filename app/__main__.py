49816473dd9cc3abd0f43077a7b528b51182044c
Joker work <deripasko@gmail.com>
2023-10-17 13:06:13 +0300

    fix

--- app/__main__.py
+++ app/__main__.py
@@ -2,10 +2,10 @@ from aiogram import Dispatcher
 from aiogram.utils import executor
 
 from app import utils
-from app.loader import dp
+from app.loader import dp, db
 from app.config import conf
 # The configuration of the modules using import
-from app import middlewares, filters, handlers
+# from app import middlewares, filters, handlers
 
 
 async def on_startup(dispatcher: Dispatcher):
@@ -14,14 +14,14 @@ async def on_startup(dispatcher: Dispatcher):
     return True
 
 
-async def on_sthutdown(dispatcher: Dispatcher):
-    await utils.setup_default_commands(dispatcher)
+async def on_sthutdown():
+    db.close()
     await utils.notify_admins("Bot shutdown", conf.tg_bot.admin_ids)
     return True
 
 if __name__ == '__main__':
-    utils.setup_logger("INFO", ["sqlalchemy.engine", "aiogram.bot.api", "aiogram.contrib.middlewares.logging"])
-    # utils.setup_logger("DEBUG", ["sqlalchemy.engine", "aiogram.bot.api"])
+    # utils.setup_logger("INFO", ["sqlalchemy.engine", "aiogram.bot.api", "aiogram.contrib.middlewares.logging"])
+    utils.setup_logger("DEBUG", ["sqlalchemy.engine", "aiogram.bot.api"])
     executor.start_polling(
         dp, on_startup=on_startup, on_shutdown=on_sthutdown, skip_updates=conf.tg_bot.skip_updates
     )
