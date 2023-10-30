41bfc5923bc243b9c8c9298d584e0de889c6e689
Joker work <deripasko@gmail.com>
2023-10-30 14:55:05 +0300

    fix

--- api/test333.py
+++ api/test333.py
@@ -7,19 +7,17 @@ bot = Bot(token="1323680830:AAHMuqFWXGsyYuab9rTXtVfjUYEd1iDm2Xc")
 dp = Dispatcher(bot)
 
 
+@dp.callback_query_handler()
+async def button_press(callback: CallbackQuery):
+    logging.info(f"{callback.message}")
+
+
 @dp.message_handler(commands=['start'])
 async def start(message: Message):
     markup = InlineKeyboardMarkup().add(
         InlineKeyboardButton(f"Text", callback_data="text")
     )
     await message.answer("text", reply_markup=markup)
-
-
-@dp.callback_query_handler()
-async def button_press(call: CallbackQuery, callback_data: dict):
-    action = callback_data.get('action')  # 1 or 2
-    floor = callback_data.get('floor')  # 2
-    logging.info(f"{action} == {floor}")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=False)
